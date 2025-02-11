c9c17b60adc20ae35ed17bb9adb79ad72aeffde6
BaekEuiHun <euihunbaek@gmail.com>
2025-02-11 23:28:00 +0900

    질문 목록 controller 생성

---
+++ src/main/java/com/example/springblog/Controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.example.springblog.Controller;
+
+import com.example.springblog.entity.Question;
+import com.example.springblog.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+
+}
