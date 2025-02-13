042d4bce5a6fdb20ca27bdb2e3b61f62dfab9091
BaekEuiHun <euihunbaek@gmail.com>
2025-02-13 22:06:23 +0900

    질문 목록 데이터 service 구현

--- src/main/java/com/example/springblog/Controller/QuestionController.java
+++ src/main/java/com/example/springblog/Controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.example.springblog.Controller;
 
 import com.example.springblog.entity.Question;
 import com.example.springblog.repository.QuestionRepository;
+import com.example.springblog.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,11 @@ import java.util.List;
 public class QuestionController {
 
     private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
