deee34dc8b169702f01cfb16ed4f889f1dc03c67
BaekEuiHun <euihunbaek@gmail.com>
2025-02-17 22:55:08 +0900

    질문 상세 페이지 controller

--- src/main/java/com/example/springblog/Controller/QuestionController.java
+++ src/main/java/com/example/springblog/Controller/QuestionController.java
@@ -3,10 +3,12 @@ package com.example.springblog.Controller;
 import com.example.springblog.entity.Question;
 import com.example.springblog.repository.QuestionRepository;
 import com.example.springblog.service.QuestionService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,6 +25,11 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 
 }
