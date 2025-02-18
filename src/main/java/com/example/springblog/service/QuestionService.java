4c2bd467ae88c3488bd2d61868202c38a1421ecc
BaekEuiHun <euihunbaek@gmail.com>
2025-02-18 22:08:07 +0900

    상세페이지 기능 service 구현

--- src/main/java/com/example/springblog/service/QuestionService.java
+++ src/main/java/com/example/springblog/service/QuestionService.java
@@ -1,12 +1,14 @@
 package com.example.springblog.service;
 
 
+import com.example.springblog.DataNotFoundException;
 import com.example.springblog.entity.Question;
 import com.example.springblog.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +18,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestionById(int id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
