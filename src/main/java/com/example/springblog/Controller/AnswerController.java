51aff182c398c78214d66e55273af1685c4b3cf4
BaekEuiHun <euihunbaek@gmail.com>
2025-04-01 22:26:13 +0900

    답변 등록 기능 구현을 위한 controller 생성

--- src/main/java/com/example/springblog/Controller/AnswerController.java
+++ src/main/java/com/example/springblog/Controller/AnswerController.java
@@ -1,11 +1,14 @@
 package com.example.springblog.Controller;
 
 import com.example.springblog.entity.Question;
+import com.example.springblog.form.AnswerForm;
 import com.example.springblog.service.AnswerService;
 import com.example.springblog.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +23,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable int id, @RequestParam String content) {
-        Question question = this.questionService.getQuestionById(id);
-        this.answerService.create(question, content);
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
+        Question question = this.questionService.getQuestion(id);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
-
     }
 }
