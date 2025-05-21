b4577330c30a7149a1aa27b7372389f3b05ef119
BaekEuiHun <euihunbaek@gmail.com>
2025-05-21 22:30:33 +0900

    중복회원 가입 방지 기능 구현 (수정2)

--- src/main/java/com/example/springblog/user/UserController.java
+++ src/main/java/com/example/springblog/user/UserController.java
@@ -43,8 +43,6 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
 
         return "redirect:/";
     }
