db396bf75119bcdbb1a7499fd32aa4a734eee6cd
BaekEuiHun <euihunbaek@gmail.com>
2025-06-17 23:03:37 +0900

    import 수정

--- src/main/java/com/example/springblog/user/UserSecurityService.java
+++ src/main/java/com/example/springblog/user/UserSecurityService.java
@@ -1,12 +1,23 @@
 package com.example.springblog.user;
 
-import lombok.RequiredArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UserSecurityService implements UserDetailsService {
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
