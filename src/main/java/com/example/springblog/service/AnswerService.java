1f7de971193067a75b861c75896100fd6c75b855
BaekEuiHun <euihunbaek@gmail.com>
2025-07-08 23:06:11 +0900

    SiteUser 객체 추가

--- src/main/java/com/example/springblog/service/AnswerService.java
+++ src/main/java/com/example/springblog/service/AnswerService.java
@@ -3,6 +3,7 @@ package com.example.springblog.service;
 import com.example.springblog.entity.Answer;
 import com.example.springblog.entity.Question;
 import com.example.springblog.repository.AnswerRepository;
+import com.example.springblog.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,12 @@ import java.time.LocalDateTime;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setQuestion(question);
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
 
     }
